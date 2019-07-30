b429cadc0acdf276677616e0c543f66b318534e1
Charlie Gildawie <charlieg@monzo.com>
2019-07-30 12:24:09 +0100

    regex filter for paths

--- render.go
+++ render.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"github.com/awalterschulze/gographviz"
+	"strings"
 )
 
 func escape(s string) string {
 	return "\"" + s + "\""
+}
+
+func basePath(u *page) string {
+	paths := strings.Split(u.url.Path, "/")
+	if len(paths) > 1 {
+		return paths[0]
+	}
+	return ""
 }
 
 func generateGraph(results []*page) string {
@@ -15,12 +25,23 @@ func generateGraph(results []*page) string {
 	_ = graph.SetDir(true)
 	_ = graph.SetStrict(true)
 
+	subGraphCache := map[string]struct{}{}
 	nodeCache := map[string]struct{}{}
 	edgeCache := map[string]struct{}{}
 
 	// We will do this in two passes to populate the nodes we correctly found
 	for _, p := range results {
-		graph.AddNode("", escape(p.url.String()), map[string]string{"label": escape(p.url.Path)})
+		subgraph := basePath(p)
+		fmt.Print(subgraph)
+		_, found := subGraphCache[subgraph]
+		if !found {
+			err := graph.AddSubGraph("Sitemap", subgraph, nil)
+			if err != nil {
+				panic(err)
+			}
+			subGraphCache[subgraph] = struct{}{}
+		}
+		graph.AddNode(subgraph, escape(p.url.String()), map[string]string{"label": escape(p.url.Path)})
 		nodeCache[p.url.String()] = struct{}{}
 	}
 
@@ -29,6 +50,12 @@ func generateGraph(results []*page) string {
 		for _, l := range p.links {
 			if _, ok := nodeCache[l.String()]; ok {
 				edgeName := p.url.String() + "-" + escape(l.String())
+
+				// bypass any backwards links
+				if len(strings.Split(p.url.String(), "/")) > len(strings.Split(l.String(), "/")) {
+					continue
+				}
+
 				// Do we have this edge already?
 				if _, ok := edgeCache[edgeName]; !ok {
 					graph.AddEdge(escape(p.url.String()), escape(l.String()), true, nil)
